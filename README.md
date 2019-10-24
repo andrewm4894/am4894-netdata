d64c9cc14ea1a508ef4a4b2dcb00825d9e0a64e5
Andrew Maguire <andrewm4894@gmail.com>
2019-10-24 21:34:02 +0100

    initial set up

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # am4894netdata
 Package for playing with netdata.
+
+## Conda
+
+Create conda env from yaml:
+```
+conda env create -f environment.yml
+```
+
+Update conda env from yaml:
+```
+conda env update -f environment.yml --prune
+```
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='am4894netdata',
+      version='0.1',
+      description='...',
+      url='http://github.com/andrewm4894/am4894netdata',
+      author='Maguire, Andrew',
+      author_email='...',
+      license='GPLv2',
+      packages=find_packages(),
+      zip_safe=False)
